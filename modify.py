0d5d16af69523db325d2915fa500dfce1793f1d2
IoT-Dashboard <140663904+IoT-Dashboard@users.noreply.github.com>
2024-05-11 00:56:16 -0400

    Modified script for input json

--- modify.py
+++ modify.py
@@ -8,6 +8,9 @@ base_new_file_name = 'new_flows'
 counter = 1
 new_file_name = f"{base_new_file_name}.json"
 
+# File name for json with mqtt information
+mqtt_file_name = "mqtt.json"
+
 # Ensuring the output file name is unique by incrementing a counter if the file already exists
 while os.path.exists(new_file_name):
     new_file_name = f"{base_new_file_name}_{counter}.json"
@@ -17,62 +20,31 @@ while os.path.exists(new_file_name):
 with open(original_file_name, 'r') as file:
     data = json.load(file)
 
-# Prompting user for initial setup information
-print(""" You will need:
-- Number of stations
-- Station names as they appear in MQTT topics
-- The bucket state MQTT topic for the first station
-""")
+# Loading mqtt information from file
+with open(mqtt_file_name, 'r') as file:
+    mqtt_info = json.load(file)
 
-# Input validation for the number of stations
-while True:
-    try:
-        num_stations = int(input("Enter the number of stations that will be displayed on the dashboard: "))
-        if 2 <= num_stations <= 12:
-            break
-        else:
-            print("Please enter an integer between 4 and 12.")
-    except ValueError:
-        print("Invalid input. Please enter an integer.")
+# Obtain the MQTT in node topics from mqtt.json
+event_manager = mqtt_info['EventManagerName']
+tracking_manager = mqtt_info['TrackingManagerName']
+stations = mqtt_info['Stations']
 
-station_names = []
-display_names = []
-
-# Collecting station names and display names with validation
-for i in range(num_stations):
-    while True:
-        station_name = input(f"\nEnter the name for station {i+1} as it appears in an MQTT TOPIC: ")
-        if ' ' not in station_name:
-            station_names.append(station_name)
-            break
-        else:
-            print("The station name cannot contain spaces.")
-
-    while True:
-        display_name = input(f"Enter the name for station {i+1} that you would like to appear on the dashboard: ")
-        if len(display_name) <= 16:
-            display_names.append(display_name)
-            break
-        else:
-            print("The display name must be 16 characters or less.")
-
-# Input validation for MQTT topic for the first station
-while True:
-    bucket_state_topic = input("\nEnter the bucket state topic for the first station: ")
-    if '/' in bucket_state_topic and ' ' not in bucket_state_topic:
-        break
-    else:
-        print("Enter a valid MQTT topic.")
-
-# Define a dictionary to map 'type' strings to topics dynamically
-topics = {
-    'Bucket State': bucket_state_topic,
-    'System Events': bucket_state_topic.replace('production/bucketstate', 'systemevents'),
-    'Tracking': bucket_state_topic.replace('production/bucketstate', 'tracking'),
-    'Likely': bucket_state_topic.replace('bucketstate', 'likely'),
-    'Production': bucket_state_topic.replace('/bucketstate', ''),
-    'Andon Events': bucket_state_topic.replace('production/bucketstate', 'andonevents')
-}
+# Create a dictionary with all the mqtt topics
+topics = []
+for station in stations:
+    event_base = f"{event_manager}/{station['LineName']}/{station['Name']}"
+    tracking_base = f"{tracking_manager}/{station['LineName']}/{station['Name']}"
+    station_topics = {
+        'Bucket State': f"{tracking_base}/production/bucketstate",
+        'Tracking': f"{tracking_base}/tracking",
+        'Likely': f"{tracking_base}/production/likely",
+        'Production': f"{tracking_base}/production",
+        'System Events': f"{event_base}/systemevents",
+        'Andon Events': f"{event_base}/andonevents",
+        'name': station['Name']
+    }
+    topics.append(station_topics)
+num_stations = len(stations)
 
 indices_to_delete = set()
 index = 0
@@ -86,28 +58,33 @@ for node in data:
     node_name = node.get('name')
     
     if node_type == 'mqtt in':
-        for key in topics:
-            if key in node_name:
-                topic = topics[key]
-                station_number = extract_station_number(node_name)
-                if station_number is not None:
-                    if station_number < num_stations:
-                        node['topic'] = topic.replace(station_names[0], station_names[station_number])
-                    else:
-                        attached_subflow = node['wires'][0][0]
-                        subflows_to_delete.add(attached_subflow)
-                        indices_to_delete.add(index)
-                else:
-                    if 'Likely' in node['name']:
-                        node['topic'] = topics['Likely']
-                    elif 'Production' in node['name']:
-                        node['topic'] = topics['Production']
+        station_number = extract_station_number(node_name)
+        
+        if station_number is not None:
+            if station_number < num_stations:
+                station_topics = topics[station_number]
+                for key in station_topics:
+                    if key in node_name:
+                        topic = station_topics[key]
+                node['topic'] = topic
+            else:
+                attached_subflow = node['wires'][0][0]
+                subflows_to_delete.add(attached_subflow)
+                indices_to_delete.add(index)
+                
+        else:
+            station_topics = topics[0]
+            if 'Likely' in node['name']:
+                node['topic'] = station_topics['Likely']
+            elif 'Production' in node['name']:
+                node['topic'] = station_topics['Production']
        
     elif node_type == 'ui_tab':
         station_number = extract_station_number(node_name)
         if station_number is not None:
             if station_number < num_stations:
-                node['name'] = display_names[station_number]
+                station_topics = topics[station_number]
+                node['name'] = station_topics['name']
             else:
                 indices_to_delete.add(index)
             
